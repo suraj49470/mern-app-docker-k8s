f4ba628c9124f47e25dd258373703e3bc647c922
Suraj Singh <suraj49470@gmail.com>
2024-04-27 12:27:09 +0530

    WIP docker

--- poll-voting-result-ui/src/App.tsx
+++ poll-voting-result-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useEffect, useReducer, useState } from 'react'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import pollReducer from './reducers/pollReducer';
@@ -7,31 +7,41 @@ import PollListContainers from './containers/PollListContainer/PollListContainer
 import { io } from "socket.io-client";
 import PollResultContainer from './containers/PollResultContainer/PollResultContainer';
 const { REACT_APP_BACKEND_URL } = process.env;
-const socket = io(REACT_APP_BACKEND_URL || 'http://localhost:5001');
-socket.on("connect", () => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-});
 
-socket.on("disconnect", () => {
-  console.log('disconnected',socket.id); // undefined
-});
 export const VoteContext = createContext<any>({} as any)
 function App() {
+  const [socket] = useState(io(REACT_APP_BACKEND_URL || 'http://localhost:5001'));
+  socket.on("connect", () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  });
+
+  socket.on("disconnect", () => {
+    console.log('disconnected', socket.id); // undefined
+  });
   const [pollState, pollDispatch] = useReducer(pollReducer, {
     poll: initialState.poll,
     currentPoll: initialState.currentPoll
   });
-  const voteStateDispatch = { pollState, pollDispatch,socket };
-    return (
-        <VoteContext.Provider value={voteStateDispatch}>
-          <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<PollListContainers />} />
-                <Route path="/result/:id" element={<PollResultContainer />} />
-            </Routes>
-          </BrowserRouter>
-      </VoteContext.Provider>
-    )
+
+  useEffect(() => {
+    if (!socket.connected) {
+      console.log('not conneted');
+      return;
+    }
+    console.log('connected');
+  }, [socket.connected]);
+
+  const voteStateDispatch = { pollState, pollDispatch, socket };
+  return (
+    <VoteContext.Provider value={voteStateDispatch}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PollListContainers />} />
+          <Route path="/result/:id" element={<PollResultContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </VoteContext.Provider>
+  )
 }
 
 export default App
--- poll-voting-ui/src/App.tsx
+++ poll-voting-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useEffect, useReducer, useState } from 'react'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import pollReducer from './reducers/pollReducer';
@@ -9,24 +9,29 @@ import createPollReducer from './reducers/createPollReducer';
 import PollContainer from './containers/PollContainer/PollContainer';
 import { io } from "socket.io-client";
 const { REACT_APP_BACKEND_URL } = process.env;
-const socket = io(REACT_APP_BACKEND_URL || 'http://localhost:5000');
-socket.on("connect", () => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-});
-
-socket.on("disconnect", () => {
-  console.log('disconnected', socket.id); // undefined
-});
 export const VoteContext = createContext<any>({} as any)
 function App() {
+  const [socket] = useState(io(REACT_APP_BACKEND_URL || 'http://localhost:5001'));
+  socket.on("connect", () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  });
+
+  socket.on("disconnect", () => {
+    console.log('disconnected', socket.id); // undefined
+  });
   const [pollState, pollDispatch] = useReducer(pollReducer, {
     poll: initialState.poll,
     currentPoll: initialState.currentPoll
   });
-
   const [createPollState, createPollDispatch] = useReducer(createPollReducer, initialState.createPoll);
   const voteStateDispatch = { pollState, pollDispatch, createPollState, createPollDispatch, socket };
-
+  useEffect(() => {
+    if (!socket.connected) {
+      console.log('not conneted');
+      return;
+    }
+    console.log('connected');
+  }, [socket.connected]);
 
 
 
