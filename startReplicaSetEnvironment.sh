58b2074cb327e57a97a19d7c88aad98279add482
Suraj Singh <suraj49470@gmail.com>
2024-05-01 22:06:49 +0530

    dsds

--- startReplicaSetEnvironment.sh
+++ startReplicaSetEnvironment.sh
@@ -15,5 +15,5 @@ echo "****** Waiting for ${DELAY} seconds to tranform URLS ******"
 sleep $DELAY
 echo "****** Transforming poll-voting-ui ******"
 docker exec -it mern-app-docker-k8s_poll-voting-ui_1 sh -x scripts/transform.sh
-echo "****** Transforming poll-voting-ui ******"
+echo "****** Transforming poll-voting-result-ui ******"
 docker exec -it mern-app-docker-k8s_poll-voting-result-ui_1 sh -x scripts/transform.sh
