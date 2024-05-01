36609f6697b3cc0c00ada7c607dfd5ed8541f1bc
Suraj Singh <suraj49470@gmail.com>
2024-05-01 21:47:10 +0530

    test

--- scripts/transform.sh
+++ scripts/transform.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo printenv
+printenv
+cd /usr/share/nginx/html/static/js
+# Iterate over all .js files in the current directory
+for filename in *.js; do
+    # Replace %REACT_APP_BACKEND_URL% with http://poll-voting-service:5000 in each .js file
+    echo "$filename";
+    sed -i 's/%REACT_APP_BACKEND_URL%/http:\/\/$REACT_APP_BACKEND_URL/g' "$filename"
+    sed -i 's/%REACT_APP_POLL_RESULT_URL%/http:\/\/$REACT_APP_POLL_RESULT_URL/g' "$filename"
+done
