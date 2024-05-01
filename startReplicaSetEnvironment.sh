6c185e6e7803ab7433a44a24811e8c9985abae2f
Suraj Singh <suraj49470@gmail.com>
2024-05-01 22:03:36 +0530

    ds

--- startReplicaSetEnvironment.sh
+++ startReplicaSetEnvironment.sh
@@ -10,3 +10,10 @@ echo "****** Waiting for ${DELAY} seconds for containers to go up ******"
 sleep $DELAY
 
 docker exec mongo_primary /scripts/rs-init.sh
+
+echo "****** Waiting for ${DELAY} seconds to tranform URLS ******"
+sleep $DELAY
+echo "****** Transforming poll-voting-ui ******"
+docker exec -it mern-app-docker-k8s_poll-voting-ui_1 sh -x scripts/transform.sh
+echo "****** Transforming poll-voting-ui ******"
+docker exec -it mern-app-docker-k8s_poll-voting-result-ui_1 sh -x scripts/transform.sh
