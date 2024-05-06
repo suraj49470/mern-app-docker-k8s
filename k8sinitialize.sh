ba10e8409d6ee0486ba6bcef2dc23bba66edc1fe
Suraj Singh <suraj49470@gmail.com>
2024-05-06 15:42:55 +0530

    sas

--- k8sinitialize.sh
+++ k8sinitialize.sh
@@ -11,17 +11,21 @@ kubectl apply -f ./k8s
 
 # Get the initial status of the pod
 pod_status=$(kubectl get pod/poll-statefulset-primary-0 -o jsonpath='{.status.phase}')
+pod_status_1=$(kubectl get pod/poll-statefulset-primary-1 -o jsonpath='{.status.phase}')
 echo "$pod_status"
+echo "$pod_status_1"
 
 # Define the while loop condition
-while [ "$pod_status" != "Running" ]; do
+while [ "$pod_status" != "Running" ] && [ "$pod_status_1" != "Running" ]; do
     # Get the status of the pod again
     pod_status=$(kubectl get pod/poll-statefulset-primary-0 -o jsonpath='{.status.phase}')
-    echo "pod status is $pod_status,wating for running state ..."
+    pod_status_1=$(kubectl get pod/poll-statefulset-primary-1 -o jsonpath='{.status.phase}')
+    echo "pod 1 status is $pod_status,wating for running state ..."
+    echo "pod 2 status is $pod_status_1,wating for running state ..."
     sleep 3 # Optionally, you can add a delay to avoid overwhelming the Kubernetes API server
 done
 echo "*****************************************"
-echo "****** check mongodb replication ********"
+echo "****** apply mongodb replication ********"
 echo "*****************************************"
 
 kubectl exec -it pod/poll-statefulset-primary-0 -- sh -x "/rs-scripts/rs-init.sh"
