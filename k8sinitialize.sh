699490c4e4f354ad8635cd2944ee1cea8298e7f6
Suraj Singh <suraj49470@gmail.com>
2024-05-06 12:43:46 +0530

    dsd

--- k8sinitialize.sh
+++ k8sinitialize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 kubectl apply -f ./poll-namespace.yml
 kubectl apply -f ./k8s
-pod_status = k get pod/poll-statefulset-primary-0 -o jsonpath='{.status.phase}'
+pod_status=k get pod/poll-statefulset-primary-0 -o jsonpath='{.status.phase}'
 echo $pod_status
