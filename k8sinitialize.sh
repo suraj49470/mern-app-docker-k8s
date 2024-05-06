56d17129bd3f2791e426fd398468231bfcfe989c
Suraj Singh <suraj49470@gmail.com>
2024-05-06 13:02:04 +0530

    wq

--- k8sinitialize.sh
+++ k8sinitialize.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+
+# Apply the namespace and Kubernetes manifests
 kubectl apply -f ./poll-namespace.yml
 kubectl apply -f ./k8s
-pod_status=${k get pod/poll-statefulset-primary-0 -o jsonpath='{.status.phase}'}
-echo $pod_status
+
+# Get the initial status of the pod
+pod_status=$(kubectl get pod/poll-statefulset-primary-0 -o jsonpath='{.status.phase}')
+echo "$pod_status"
+
+# Define the while loop condition
+while [ "$pod_status" != "Running" ]; do
+    # Get the status of the pod again
+    pod_status=$(kubectl get pod/poll-statefulset-primary-0 -o jsonpath='{.status.phase}')
+    echo "$pod_status"
+    sleep 3 # Optionally, you can add a delay to avoid overwhelming the Kubernetes API server
+done
+
+echo "Loop finished"
