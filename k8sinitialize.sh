df54d94d533591cf3907672fda3802d94302a871
Suraj Singh <suraj49470@gmail.com>
2024-05-07 14:11:03 +0530

    sas

--- k8sinitialize.sh
+++ k8sinitialize.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
-kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
+kubectl delete --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+#kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
 # Apply the namespace and Kubernetes manifests
 kubectl apply -f ./poll-namespace.yml
 kubectl config set-context --current --namespace=poll
@@ -32,7 +32,7 @@ echo "*****************************************"
 echo "****** apply mongodb replication ********"
 echo "*****************************************"
 
-kubectl exec -it pod/poll-statefulset-primary-0 -- sh -x "/rs-scripts/rs-init.sh"
+# kubectl exec -it pod/poll-statefulset-primary-0 -- sh -x "/rs-scripts/rs-init.sh"
 kubectl get all -o wide
 
 
