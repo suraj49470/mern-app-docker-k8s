80fdfcf35b21b2199e570c55a9b151351f681d1b
Suraj Singh <suraj49470@gmail.com>
2024-05-07 14:53:50 +0530

    sas

--- k8sinitialize.sh
+++ k8sinitialize.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-kubectl delete --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
-#kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
+kubectl apply --filename https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
 # Apply the namespace and Kubernetes manifests
 kubectl apply -f ./poll-namespace.yml
 kubectl config set-context --current --namespace=poll
