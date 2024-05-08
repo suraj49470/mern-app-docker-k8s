158f4428d3b927d9999fbcd14dcd1370f9160a1b
Suraj Singh <suraj49470@gmail.com>
2024-05-08 11:10:50 +0530

    mongo replication done

--- k8sinitialize.sh
+++ k8sinitialize.sh
@@ -8,5 +8,13 @@ kubectl apply -f ./poll-namespace.yml
 kubectl config set-context --current --namespace=poll
 sleep 3
 kubectl apply -f ./k8s
-
+echo "waiting for POD creation..."
+sleep 10
+echo "wating for mongo primary and secondary db"
+kubectl wait --namespace poll --for=condition=ready pod --selector=role=mongo-primary --timeout=180s
+kubectl wait --namespace poll --for=condition=ready pod --selector=role=mongo-secondary --timeout=180s
+echo "mongo primary and secondary db setup completed"
+echo "configuring replicaset on primary mongo pod"
+kubectl exec -it pod/poll-statefulset-primary-0 -- sh -x rs-scripts/rs-init.sh
+kubectl get all -o wide
 
