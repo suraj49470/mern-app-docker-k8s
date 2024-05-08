c498a6adcd3e13de9be5f591904ff2c4a667e174
Suraj Singh <suraj49470@gmail.com>
2024-05-08 12:07:42 +0530

    dsd

--- k8sinitialize.sh
+++ k8sinitialize.sh
@@ -8,8 +8,6 @@ kubectl apply -f ./poll-namespace.yml
 kubectl config set-context --current --namespace=poll
 sleep 3
 kubectl apply -f ./k8s
-echo "waiting for POD creation..."
-sleep 10
 echo "wating for mongo primary and secondary db"
 kubectl wait --namespace poll --for=condition=ready pod --selector=role=mongo-primary --timeout=180s
 kubectl wait --namespace poll --for=condition=ready pod --selector=role=mongo-secondary --timeout=180s
